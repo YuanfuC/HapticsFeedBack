1b526f8bb97208a60d62722974a5e8447f9c8893
陈远孚 <chenyuanfu@zerozero.cn>
2018-02-22 11:38:00 +0800

    Feed back demo.

--- FeedbackGen/ViewController.m
+++ FeedbackGen/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
 
-@interface ViewController ()
+@interface ViewController ()<UIGestureRecognizerDelegate>
 
 @end
 
@@ -16,14 +17,72 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)playSyetemSound1 {
+    AudioServicesPlaySystemSound(1519);
 }
 
+- (IBAction)playSyetemSound2 {
+    AudioServicesPlaySystemSound(1520);
+}
+
+- (IBAction)playSyetemSound3 {
+    AudioServicesPlaySystemSound(1521);
+}
+
+- (IBAction)impactFeedbackLight {
+    UIImpactFeedbackGenerator *impactFBG = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleLight];
+    [impactFBG prepare];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [impactFBG impactOccurred];
+    });
+}
+
+- (IBAction)impactFeedbackMedium {
+    UIImpactFeedbackGenerator *impactFBG = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleMedium];
+    [impactFBG prepare];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [impactFBG impactOccurred];
+    });
+}
+
+- (IBAction)impactFeedbackHeavy {
+    UIImpactFeedbackGenerator *impactFBG = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleHeavy];
+    [impactFBG prepare];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [impactFBG impactOccurred];
+    });
+}
+
+- (IBAction)selectionFeedBackChange {
+    UISelectionFeedbackGenerator *selelctionFBG = [[UISelectionFeedbackGenerator alloc] init];
+    [selelctionFBG prepare];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [selelctionFBG selectionChanged];
+    });
+}
+
+- (IBAction)notifactionfeadbackSuccess {
+    UINotificationFeedbackGenerator *notificationFBG = [[UINotificationFeedbackGenerator alloc] init];
+    [notificationFBG prepare];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [notificationFBG notificationOccurred:UINotificationFeedbackTypeSuccess];
+    });
+}
+- (IBAction)notifactionfeadbackWarning {
+    UINotificationFeedbackGenerator *notificationFBG = [[UINotificationFeedbackGenerator alloc] init];
+    [notificationFBG prepare];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [notificationFBG notificationOccurred:UINotificationFeedbackTypeWarning];
+    });
+}
+- (IBAction)notifactionfeadbackError {
+    UINotificationFeedbackGenerator *notificationFBG = [[UINotificationFeedbackGenerator alloc] init];
+    [notificationFBG prepare];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [notificationFBG notificationOccurred:UINotificationFeedbackTypeError];
+    });
+}
 
 @end
